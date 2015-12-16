7f08116772b0695cebd0e1b9c037b8d6025b7872
jcjonathan <jcjonathancj@gmail.com>
2015-12-16 22:56:44 +0000

    got first pizza place name

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() { 
-    $.get("https://api.foursquare.com/v2/venues/search" +
-        "?client_id=" + client_id + "U1HU2J5EUBPNFN1TMJDFZCH3ZTFEHXKOH2P55A5GCR1P53MM" 
-        "&client_secret=" + client_secret + "EE1ELZUEEFRVTNGNP4HUEMDK3HHNN0KH5IDTKVMDHNAWJ1LM"
+ $.get("https://api.foursquare.com/v2/venues/search" +
+        "?client_id=" + "U1HU2J5EUBPNFN1TMJDFZCH3ZTFEHXKOH2P55A5GCR1P53MM" +
+        "&client_secret=" + "EE1ELZUEEFRVTNGNP4HUEMDK3HHNN0KH5IDTKVMDHNAWJ1LM" +
         "&v=20151209" +
-        "&near=nyc"
+        "&near=nyc" + 
         "&query=pizza",
         function(data) {
-            // Callback code goes here. This gets executed after receiving data from the Foursquare API.
+            console.log(data["response"][""]);
         });
+        
 });
